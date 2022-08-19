aa76b6f19e912b47407a2d703d9d46a292c2c34c
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-08-19 14:44:41 -0400

    modified:   README.md 	modified:   src/PirateUtils.jl

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 [![Build Status](https://github.com/jlapeyre/PirateUtils.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/jlapeyre/PirateUtils.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/jlapeyre/PirateUtils.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/jlapeyre/PirateUtils.jl)
+
+
+Some useful code that incidentally commits type piracy.
+
+* `f^n` returns the function `(x, args...) -> f(f(f(x, args...), args...), args...)...`, where `f` is nested `n` times. This is taken from a post by oxinabox.
+
+* `(construct a generator)()`. Calling a generator, iterates over the generator, discarding all values.
--- src/PirateUtils.jl
+++ src/PirateUtils.jl
@@ -1,5 +1,36 @@
 module PirateUtils
 
-# Write your package code here.
+"""
+    (f^n)
+
+Return a function that makes `n` calls to function `f`. When calling
+`f^n`, the output
+of each call to `f` is passed as the first argument to the next call.
+Remaining arguments are passed unchanged on each iteration
+
+# Examples
+Find a solution to `x == cos(x)`
+
+```
+julia> x = (cos^100)(1.0)
+0.7390851332151607
+
+julia> cos(x) == x
+true
+```
+"""
+function Base.:(^)(f::Function, n::Integer)
+    iszero(n) && return (x, args...) -> x
+    n < 0 && throw(DomainError(n, "n must be non-negative"))
+    return function(x, args...)
+        for i in 1:n
+            x=f(x, args...)
+        end
+        x
+    end
+end
+
+
+(g::Base.Generator)(f=identity) = foreach(identity, g)
 
 end
